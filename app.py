ab3087d081190f2821d7fa3d39b54812bf4d3c6a
Taisei Matsuoka <bc181743@ipu-japan.ac.jp>
2021-07-31 20:53:44 +0900

    flask

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask,render_template,request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def getvalue():
+    name = request.form['name']
+    age = request.form['age']
+    db = request.form['dateofbirth']
+    return render_template('pass.html', n=name, age=age, db=db)
